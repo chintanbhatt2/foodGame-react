c3613fdca30f6f2114cb938dae135fcec25e9c42
wargen <wargenng@gmail.com>
2020-01-31 15:29:55 -0800

    placing the initial screen into the app component

--- food-game/src/App.js
+++ food-game/src/App.js
@@ -4,9 +4,46 @@ import "./App.css";
 function App() {
   return (
     <div className="app-header">
-      <h1>Food Game</h1>
+      <h1 className="app-title">Food Game</h1>
+      <InitialScreen />
     </div>
   );
+}
+
+class InitialScreen extends React.Component {
+  render() {
+    return (
+      <div className="initial-screen">
+        <p>
+          Player amount:
+          <input
+            type="number"
+            className="firstBox playerAmount"
+            name="quantity"
+            min="2"
+            max="10"
+          />
+        </p>
+        <p>
+          Choices per player:
+          <input
+            type="number"
+            className="firstBox choicesPer"
+            name="quantity"
+            min="1"
+            max="3"
+          />
+        </p>
+        <button className="firstBtn" onclick={InitialSubmit}>
+          SUBMIT
+        </button>
+      </div>
+    );
+  }
+}
+
+function InitialSubmit() {
+  alert("AHHHHHH");
 }
 
 export default App;
